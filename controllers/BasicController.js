22f2bb5be8f3825ac1dadb408017c0a5fe5761ce
S-Kaluza <s.kaluza02@gmail.com>
2023-01-20 09:13:59 +0100

    change

--- controllers/BasicController.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    home: (req, res) => {
-      res.send('This is the home page');
-    },
-    second: (req, res) => {
-      res.send('This is the second page');
-    },
-  };
